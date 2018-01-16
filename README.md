638494a8ab8d883466a920038fca4c167438dc70
Jouni Leino <jouni.leino@siili.com>
2018-01-16 18:52:04 +0100

    Function now passes data to documentDB

--- README.md
+++ README.md
@@ -10,36 +10,40 @@
 
 Serverless
 
+```
 npm install serverless -g
+```
 
 https://serverless.com
 
-
-Finch plugin - For AWS S3 static website deployment
-
-npm install --save serverless-finch
-
-https://www.npmjs.com/package/serverless-finch
 
 
 #### Deploy backend
 
 Deploy backend (API, Functions & Database)
 
-sls deploy
+```
+serverless deploy
+```
 
 Deploy only the lambda function
 
-serverless deploy function -f myservice
+```
+sls deploy function -f hello
+```
 
+Test function
 
-
+```
+curl -X POST https://cloudquiz.azurewebsites.net/api/hello -H "Content-Type: application/json" -d '{ "id":"4444", "name":"jouni" }' 
+```
 
 
 ### Frontend
 
 #### Install tools
 
+```
 npm install -g vue-cli
 
 vue init webpack-simple myservice
@@ -47,15 +51,28 @@ vue init webpack-simple myservice
 cd myservice
 
 npm install
+```
 
 #### Run locally
 
+```
 npm run dev
+```
 
 #### Build distribution
 
+```
 npm run build
+```
 
 #### Deploy frontend
 
+```
 sls client deploy
+```
+
+
+
+## Author
+
+[Jouni Leino](https://github.com/jounile)
--- handler.js
+++ handler.js
@@ -1,14 +1,26 @@
+// handler.js
+
 'use strict';
 
 /* eslint-disable no-param-reassign */
 
-module.exports.hello = function (context) {
-  context.log('JavaScript HTTP trigger function processed a request.');
+module.exports.hello = function (context, req) {
+  
+  context.log("Request Headers = " + JSON.stringify(req.headers));
+  context.log("Request Body = " + JSON.stringify(req.body));
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: 'Go Serverless v1.x! Your function executed successfully!',
-  };
+  const query = req.query; // dictionary of query strings
+  const body = req.body; // Parsed body based on content-type
+  const method = req.method; // HTTP Method (GET, POST, PUT, etc.)
+  const originalUrl = req.originalUrl; // Original URL of the request - https://myapp.azurewebsites.net/api/foo?code=sc8Rj2a7J
+  const headers = req.headers; // dictionary of headers
+  const params = req.params; // dictionary of params from URL
+  const rawBody = req.rawBody; // unparsed body
+
+  context.bindings.record = {
+      id: req.body.id,
+      name: req.body.name
+  }
 
   context.done();
 };
