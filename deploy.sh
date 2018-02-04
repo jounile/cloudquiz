809ad68b68eb2ec3d3a7d51c310a3e2462ffcf76
Jouni Leino <jouni.leino@siili.com>
2018-02-04 01:22:13 +0100

    Shortening resource group name

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Create a resource group.
-az group create --name CloudquizResourceGroup --location "West Europe"
+az group create --name Cloudquiz-rg --location "West Europe"
 
 # Deploy ARM template.
 az group deployment create \
     --name CloudquizDeployment \
-    --resource-group CloudquizResourceGroup \
+    --resource-group Cloudquiz-rg \
     --template-file azuredeploy.json \
     --parameters @azuredeploy.parameters.json
 
